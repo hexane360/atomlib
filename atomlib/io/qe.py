c4391990fd6072113c28b6e77a8538fa4dcdc12a
Colin Gilgenbach <colin@gilgenbach.net>
2024-04-07 15:00:57 -0400

    Fixed QE output

--- atomlib/io/qe.py
+++ atomlib/io/qe.py
@@ -4,24 +4,39 @@ import typing as t
 
 import polars
 
+from ..atoms import _get_symbol_mapping
 from ..atomcell import HasAtomCell
 from ..util import FileOrPath, open_file
 
 
 def write_qe(atomcell: HasAtomCell, f: FileOrPath, pseudo: t.Optional[t.Mapping[str, str]] = None):
+    """
+    Write a structure to a Quantum Espresso pw.x file.
+
+    Args:
+      atomcell: Structure to write
+      f: File or path to write to
+      pseudo: Mapping from atom symbol
+    """
     if not isinstance(atomcell, HasAtomCell):
         raise TypeError("'qe' format requires an AtomCell.")
 
-    atoms = atomcell.wrap().get_atoms('cell_frac').with_mass()
+    atoms = atomcell.wrap().get_atoms('cell_box').with_mass()
 
     types = atoms.select(('symbol', 'mass')).unique(subset='symbol').sort('mass')
     if pseudo is not None:
-        types = types.with_columns(polars.col('symbol').cast(polars.Utf8).map_dict(dict(pseudo), return_dtype=polars.Utf8).alias('pot'))
+        types = types.with_columns(_get_symbol_mapping(types, pseudo, ty=polars.Utf8).alias('pot'))
     else:
-        types = types.with_columns(polars.col('symbol').apply(lambda sym: f"{sym}.UPF").alias('pot'))
+        types = types.with_columns((polars.col('symbol') + polars.lit('.UPF')).alias('pot'))
+        #types = types.with_columns(polars.col('symbol').apply(lambda sym: f"{sym}.UPF").alias('pot'))
 
     with open_file(f, 'w') as f:
-        print(f"&SYSTEM ibrav=0 nat={len(atoms)} ntyp={len(types)}", file=f)
+        print(f"""\
+&SYSTEM
+  ibrav=0,
+  nat={len(atoms)},
+  ntyp={len(types)}
+/""", file=f)
 
         ortho = atomcell.get_transform('local', 'cell_box').to_linear().inner
         print(f"\nCELL_PARAMETERS angstrom", file=f)
@@ -35,3 +50,5 @@ def write_qe(atomcell: HasAtomCell, f: FileOrPath, pseudo: t.Optional[t.Mapping[
         print(f"\nATOMIC_POSITIONS crystal", file=f)
         for (symbol, (x, y, z)) in atoms.select(('symbol', 'coords')).rows():
             print(f"{symbol:>4} {x:.8f} {y:.8f} {z:.8f}", file=f)
+
+        print(file=f)  # allows for easy concatenation
